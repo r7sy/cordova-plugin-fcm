c0d53295a130577568cc38b8f797d7c05300d376
Radwan Diab <sy-vip@live.com>
2017-03-02 07:21:01 -0800

    Update AlarmReceiver.java

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -4,13 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
-
+import android.util.Log;
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context arg0, Intent arg1) {
         // For our recurring task, we'll just display a message
         Toast.makeText(arg0, "I'm running", Toast.LENGTH_SHORT).show();
+        Log.d("t","running recurrent");
 
     }
 
