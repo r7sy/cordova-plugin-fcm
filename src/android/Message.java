dc7d4625d72fd874798d35920af68df10cb90d4e
Radwan Diab <sy-vip@live.com>
2017-03-13 06:18:38 -0700

    Update Message.java

--- src/android/Message.java
+++ src/android/Message.java
@@ -5,12 +5,15 @@ public class Message {
 	private String title;
 	private String body;
 	private String senderId;
+	
+	private String senderName;
 	private Date arrivalTime;
-	public Message(String id , String title , String body , String senderId,String arrivalTime)
+	public Message(String id , String title , String body , String senderId,String senderName,String arrivalTime)
 	{ 
 	this.id=id;
 	this.body=body;
 	this.senderId=senderId;
+	this.senderName =senderName;
 	if(arrivalTime==null)
 	{
 		this.arrivalTime=new Date();
@@ -27,7 +30,7 @@ public class Message {
 	public String getBody(){return this.body;}
 	public String getSenderId(){return this.senderId;}
 	public Long getArrivalTime(){return this.arrivalTime.getTime();}
-	
+	public String getSenderName(){return this.senderName;};
 	
 	
 }
