0edfa0d5ba158e655863bf13c735d8081aa9ca37
Radwan Diab <sy-vip@live.com>
2017-03-21 08:28:21 -0700

    Update Sender.java

--- src/android/Sender.java
+++ src/android/Sender.java
@@ -2,16 +2,18 @@ package com.gae.scaffolder.plugin;
 public class Sender {
 	private String id;
 	private String sound ;
-	public Sender(String id , String sound)
+	private boolean muted ;
+	public Sender(String id , String sound,boolean muted)
 	{
 		this.id=id;
 		this.sound=sound;
-		
+		this.muted =muted;
 	}
 	
 	public String getId(){return this.id;}
 	public String getSound(){return this.sound;}
 	public void setSound(String s){this.sound=s;}
-	
+	public boolean getMuted(return this.muted;)
+	public void setMuted (boolean muted){this.muted = muted;}
 	
 }
