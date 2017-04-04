b8f6f1d85cd4750db2cd368e0a413e90d3ed3a5e
Radwan Diab <sy-vip@live.com>
2017-04-04 08:57:18 -0700

    Update Message.java

--- src/android/Message.java
+++ src/android/Message.java
@@ -6,17 +6,21 @@ public class Message {
 	private String body;
 	private String senderId;
 	
-	private String thumbnail;
+	private String thumbnail_url;
+	
+	private String hash;
 	private String senderName;
 	private Date arrivalTime;
-	public Message(String id , String title , String body , String senderId,String senderName,String thumbnail,String arrivalTime)
+	public Message(String id , String title , String body , String senderId,String senderName,String thumbnail_url,String tumbnail_hash,String arrivalTime)
 	{ 
 	this.id=id;
 	this.title=title;
 	this.body=body;
 	this.senderId=senderId;
 	this.senderName =senderName;
-	this.thumbnail = thumbnail;
+	this.thumbnail_url = thumbnail_url;
+	
+	this.thumbnail_hash = thumbnail_hash;
 	if(arrivalTime==null)
 	{
 		this.arrivalTime=new Date();
@@ -35,6 +39,7 @@ public class Message {
 	public Long getArrivalTime(){return this.arrivalTime.getTime();}
 	public String getSenderName(){return this.senderName;};
 
-	public String getThumbnail(){return this.thumbnail;}
+	public String getThumbnailHash(){return this.thumbnail_hash;}
+	public String getThumbnailUrl(){return this.thumbnail_url;}
 	
 }
