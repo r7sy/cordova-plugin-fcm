6f2e241fa6b4c1e1f4dd5b61221ed941e0a359c3
Radwan Diab <sy-vip@live.com>
2017-04-22 13:50:48 -0700

    Update AppDelegate+FCMPlugin.m

--- src/ios/AppDelegate+FCMPlugin.m
+++ src/ios/AppDelegate+FCMPlugin.m
@@ -159,8 +159,13 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
     }
 
     NSLog(@"PushCredentials: %@", [credentials token]);
-	lastToken=  [[NSString alloc] initWithData:[credentials token]
-                                         encoding:NSASCIIStringEncoding] ;
+	const unsigned *tokenBytes = [credentials.token bytes];
+    lastToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                         ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
+                         ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
+                         ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
+
+	
 	 [FCMPlugin.fcmPlugin notifyOfTokenRefresh:lastToken];
 }
 
