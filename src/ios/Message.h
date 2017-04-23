26adaf595b2e468ea03b41dadbf5aa18eb232047
Radwan Diab <sy-vip@live.com>
2017-04-23 10:09:25 -0700

    Create Message.h

---
+++ src/ios/Message.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+@interface Message:NSObject
+
+@property(nonatomic, readwrite) NSString * id;
+@property(nonatomic, readwrite) NSString * title;
+@property(nonatomic, readwrite) NSString * body;
+@property(nonatomic, readwrite) NSString * senderId;
+@property(nonatomic, readwrite) NSString * thumbnail_url;
+@property(nonatomic, readwrite) NSString * thumbnail_hash;
+@property(nonatomic, readwrite) NSString * senderName;
+
+-(NSDictionary *) getDict;
+-(id) initWithDict :(NSDictionary *) dict ;
+@end
