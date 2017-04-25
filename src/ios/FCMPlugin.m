844494f8db3a69e7e788347a625c01fd5876810c
Radwan Diab <sy-vip@live.com>
2017-04-25 09:44:46 -0700

    Update FCMPlugin.m

--- src/ios/FCMPlugin.m
+++ src/ios/FCMPlugin.m
@@ -53,74 +53,22 @@ static FCMPlugin *fcmPluginInstance;
 	NSMutableArray* arr=[[NSMutableArray alloc] init];
 	[arr addObject:[m getDict]];
 	[arr addObject:[m1 getDict]];*/
-	NSDictionary *dict = @{@"id":@"1",@"sound":@"default",@"muted":[[NSNumber alloc] initWithBool:NO],@"vibrate":[[NSNumber alloc] initWithBool:YES]};
-	Sender* s = [[Sender alloc] initWithDict:dict];
-	NSMutableArray* arr=[[NSMutableArray alloc] init];
-	[arr addObject:[s getDict]];
-	//NSLog(@"got message dict %s",[m getDict]);
-	if ([NSJSONSerialization isValidJSONObject:arr])
-{
-  // Serialize the dictionary
- NSError *error;
-  NSData *json = [NSJSONSerialization dataWithJSONObject:arr options:NSJSONWritingPrettyPrinted error:&error];
-  NSLog(@"serilized array");
-  // If no errors, let's view the JSON
-  if (json != nil && error == nil)
-  {
-    NSString *jsonString = [[NSString alloc] initWithData:json encoding:NSUTF8StringEncoding];
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-NSString *documentsDirectory = [paths objectAtIndex:0];
-NSString *path = [documentsDirectory stringByAppendingPathComponent:@"/NoCloud/messages.json"];
-
-    NSLog(@"JSON: %@", jsonString);
-	[jsonString writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:&error];
-         
-  }
-}
-	[AppDelegate writeFile:@"test.txt":@"hello my test":NO];
+	
+	[AppDelegate writeFile:@"test.txt":@"hello my test":yes];
+	[AppDelegate writeFile:@"test.txt":@"hello my test":yes];
+	[AppDelegate writeFile:@"test.txt":@"hello my test":yes];
+	NSLog([AppDelegate readFile:@"test.txt"]	);
+	[AppDelegate writeJSONFile:@"test.json":arr];
+	NSLog([AppDelegate readFile:@"test.json"]	);
+	NSMutableArray* newar=[AppDelegate readJSONFile:@"test.json"];
+	for(int i=0;i<[newar count];i++)
+	{
+	NSLog(newar[i].body);
+	
+	}
         NSString* token = [AppDelegate getLastToken];
 		token =@"hello";
 		NSLog(@"got last token %@", token);
-NSError *error;		
-NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-NSString *documentsDirectory = [paths objectAtIndex:0];
-NSString *path = [documentsDirectory stringByAppendingPathComponent:@"/NoCloud/messages.json"];
-//[token writeToFile:path atomically:YES];
-token = [NSString stringWithFormat: @"%@\n", token];
-NSFileManager *fileManager = [NSFileManager defaultManager];
-/*if(![fileManager fileExistsAtPath:path])
-{  NSLog(@"file doesn't exist");
-
-  [token writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:&error];
-}
-else
-{NSLog(@"file doesn't exists");
-  NSFileHandle *myHandle = [NSFileHandle fileHandleForWritingAtPath:path];
-  [myHandle seekToEndOfFile];
- [myHandle writeData:[token dataUsingEncoding:NSUTF8StringEncoding]];
-}*/
-
-NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-NSLog(@"file content %@",content);
-/*content=[content stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-  NSArray *array = [content componentsSeparatedByString:@"\n"];
-  //NSLog(@"file array last object %@",[array lastObject]);
-  int i;
- for (i = 0; i < [array count]; i++) {
-   NSString* arrayElem = [array objectAtIndex:i];
-  NSLog(@"file array %d object %@",i,arrayElem);
- }*/
- /*NSData * jsonData = [NSData dataWithContentsOfFile:path];
- id object = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:&error];
-  if ([object isKindOfClass:[NSDictionary class]] && error == nil)
-{
-  NSLog(@"dictionary: %@", object);
-  
-  //Message* m2 = [[Message alloc] initWithDict:object withDate:[[NSDate alloc] initWithTimeIntervalSince1970:[object[@"arrivalTime"] doubleValue]]];
-	//NSLog(@"message: %@", [m2 body]);
-  
-  
-  }*/
         CDVPluginResult* pluginResult = nil;
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:token];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
