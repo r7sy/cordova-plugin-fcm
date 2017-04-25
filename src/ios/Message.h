26ff6f423f23c3e294af588c26064d63005c4798
Radwan Diab <sy-vip@live.com>
2017-04-25 05:24:25 -0700

    Update Message.h

--- src/ios/Message.h
+++ src/ios/Message.h
@@ -9,7 +9,7 @@
 @property(nonatomic, readwrite) NSString * thumbnail_url;
 @property(nonatomic, readwrite) NSString * thumbnail_hash;
 @property(nonatomic, readwrite) NSString * senderName;
-@property(nonatomic, readwrite) NSNumber * muted;
+@property(nonatomic, readwrite) NSDate  * arrivalTime;
 -(NSDictionary *) getDict;
 -(id) initWithDict :(NSDictionary *) dict ;
 @end
